a7af6b24ccfdfafde1f5941ce281bcfb8935b12e
ben-gro <ben.grobecker@tu-dortmund.de>
2022-05-12 14:11:00 +0200

    Auswertung Teil 2

--- V70/drehschieber.py
+++ V70/drehschieber.py
@@ -9,11 +9,11 @@ from uncertainties import ufloat
 # (Nicht nötig mit Makefile aus dem Toolbox-Workshop)
 
 # Evakuierungskurve
-t, dt, p1, p2, p3, dp = np.genfromtxt('data/dreh_evac.csv', unpack=True)
+t, dt, p1, dp1, p2, dp2, p3, dp3 = np.genfromtxt('data/dreh_evac.csv', unpack=True)
 T = unp.uarray(t,dt)
-p_1 = unp.uarray(p1,dp)
-p_2 = unp.uarray(p2,dp)
-p_3 = unp.uarray(p3,dp)
+p_1 = unp.uarray(p1,dp1)
+p_2 = unp.uarray(p2,dp2)
+p_3 = unp.uarray(p3,dp3)
 P= np.mean([p_1,p_2,p_3], axis=0)
 
 # Messdaten zu LateX-Tabelle
@@ -51,7 +51,7 @@ print(f'Abschnitt 3: m = {uParams3[0]:S}, b = {uParams3[1]:S}\n')
 # Plot logarithmisch
 x = np.linspace(0,600,6000)
 fig, ax = plt.subplots()
-ax.errorbar(t, noms(P), xerr=0.2, yerr=stds(P), fmt='k+', ecolor='r', label=r'Messwerte mit Fehlerbalken')
+ax.errorbar(t, noms(P), xerr=0.2, yerr=stds(P), fmt='k+', ecolor='r', capsize=2, label=r'Messwerte mit Fehlerbalken')
 ax.plot(x[0:3150], params1[0]*x[0:3150] + params1[1], label=r'Abschnitt 1')
 ax.plot(x[0:5500], params2[0]*x[0:5500] + params2[1], label=r'Abschnitt 2')
 ax.plot(x, params3[0]*x + params3[1], label=r'Abschnitt 3')
@@ -64,17 +64,17 @@ plt.tight_layout()
 plt.savefig('abb/dreh_evac.pdf')
 
 # Leckratenmessung
-t05, dt05, p05_1, p05_2, p05_3, dp05 = np.genfromtxt('data/dreh_leck0.5.csv', unpack=True)
-t10, dt10, p10_1, p10_2, p10_3, dp10 = np.genfromtxt('data/dreh_leck10.csv', unpack=True)
-t50, dt50, p50_1, p50_2, p50_3, dp50 = np.genfromtxt('data/dreh_leck50.csv', unpack=True)
-t100, dt100, p100_1, p100_2, p100_3, dp100 = np.genfromtxt('data/dreh_leck100.csv', unpack=True)
+t05, dt05, p05_1, dp05_1, p05_2, dp05_2, p05_3, dp05_3 = np.genfromtxt('data/dreh_leck0.5.csv', unpack=True)
+t10, dt10, p10_1, dp10_1, p10_2, dp10_2, p10_3, dp10_3 = np.genfromtxt('data/dreh_leck10.csv', unpack=True)
+t50, dt50, p50_1, dp50_1, p50_2, dp50_2, p50_3, dp50_3 = np.genfromtxt('data/dreh_leck50.csv', unpack=True)
+t100, dt100, p100_1, dp100_1, p100_2, dp100_2, p100_3, dp100_3 = np.genfromtxt('data/dreh_leck100.csv', unpack=True)
 
 # Messdaten Dictionary
 dict = {'t': unp.uarray(t05,dt05),
-    '0.5': [unp.uarray(p05_1, dp05), unp.uarray(p05_2, dp05), unp.uarray(p05_2, dp05)],
-    '10': [unp.uarray(p10_1, dp10), unp.uarray(p10_2, dp10), unp.uarray(p10_2, dp10)],
-    '50': [unp.uarray(p50_1, dp50), unp.uarray(p50_2, dp50), unp.uarray(p50_2, dp50)],
-    '100': [unp.uarray(p100_1, dp100), unp.uarray(p100_2, dp100), unp.uarray(p100_2, dp100)]}
+    '0.5': [unp.uarray(p05_1, dp05_1), unp.uarray(p05_2, dp05_2), unp.uarray(p05_3, dp05_3)],
+    '10': [unp.uarray(p10_1, dp10_1), unp.uarray(p10_2, dp10_2), unp.uarray(p10_3, dp10_3)],
+    '50': [unp.uarray(p50_1, dp50_1), unp.uarray(p50_2, dp50_2), unp.uarray(p50_3, dp50_3)],
+    '100': [unp.uarray(p100_1, dp100_1), unp.uarray(p100_2, dp100_2), unp.uarray(p100_3, dp100_3)]}
 
 
 # Messdaten zu LateX-Tabelle
@@ -91,4 +91,23 @@ with open('content/dreh_leck_mean.tex', 'w') as f:
     for i in range(dict['t'].size):
        f.write(f'{dict["t"][i]:S} && {dict["0.5"][i]:S} && {dict["10"][i]:S} {dict["50"][i]:S}  && {dict["100"][i]:S} \\\\ \n')
 
+# Ausgleichsrechnung
 
+# Plot
+for p in ['0.5', '10', '50', '100']:
+    params,cov_matrix = np.polyfit(noms(dict['t']), noms(dict[p]), deg=True, cov=True)
+    uParams = unp.uarray(params, np.sqrt(np.diag(cov_matrix)))
+    print(f'{p}: m = {uParams[0]:S}, b = {uParams[1]:S}')
+
+    x = np.linspace(0,200,2000)
+
+    fig, ax = plt.subplots()
+    ax.errorbar(noms(dict['t']), noms(dict[p]), xerr=stds(dict['t']), yerr=stds(dict[p]), fmt='k+', ecolor='r', capsize=2, label=r'Messwerte mit Fehlerbalken')
+    ax.plot(x, params[0]*x + params[1], label=r'Ausgleichsgerade')
+    
+    ax.set_xlabel(r't/\si{\second}')
+    ax.set_ylabel(r'p/\si{\milli\bar}')
+    ax.grid()
+    ax.legend(loc='best')
+    fig.tight_layout()
+    fig.savefig('abb/dreh_leck' + p + '.pdf')
